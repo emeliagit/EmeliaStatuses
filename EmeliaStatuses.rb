217c25af0e6e671e865d43e3e61e5b90544db2eb
Emeliaisdumb <66752943+emeliagit@users.noreply.github.com>
2023-03-20 10:16:10 -0400

    Add actual script

---
+++ EmeliaStatuses.rb
@@ -0,0 +1,26 @@
+# Introduction and choices list
+puts "Hello! This here's a digitized list of my favorite statuses/status ideas I've had so far! Which would you like to see?"
+puts "(1), (2), (3), (4), (5)"
+# get user choice
+userchoice = gets.chomp
+# switch statement to assign different choices to different results
+case userchoice
+	# results
+	
+	
+	when "1"
+		puts "God, I'm so gay. Big Lesbo moment. Massive unga bunga ooga booga time, hummina hummina."
+		puts "My masterpiece. My Mona Lisa, a work which will surely stand the test of time. Truly wonderful."
+	when "2"
+		puts "I may be Emelia: Liker of Girls, but why can't I be Emelia: Kisser of Girls?"
+		puts "But god, is it not true? If only, my friend, if only."
+	when "3"
+		puts "OwO fucko boingo"
+		puts "Just a simple funny. What I name all of my Dark Souls characters, or a variation of it."
+	when "4"
+		puts "Queer. The queerest of them all."
+		puts "I mean... pretty great, innit? It's true, I think. I'm pretty darn queer."
+	when "5"
+		puts "God, I'm gay."
+		puts "Simple, but effective. True, and really gets the point across."
+end
